43bd3f8ad63059da9337bb5f14de26de8f9adea4
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2022-08-31 08:03:05 -0300

    Configurações do mapa: modo claro/escuro

--- screens/AnimalLocation/index.js
+++ screens/AnimalLocation/index.js
@@ -1,4 +1,4 @@
-import { Dimensions, Text, View } from "react-native"
+import { Dimensions, Text, useColorScheme } from "react-native"
 import {
   SafeAreaViewContent,
   ViewMenu,
@@ -8,15 +8,21 @@ import MapView from "react-native-maps"
 import ReportedAnimalSignalMarker from "../../components/ReportedAnimalSignalMarker"
 import { ReportedAnimalPreDetails } from "../../components/ReportedAnimalPreDetails"
 import { AnimalLocationProvider } from "../../contexts/AnimalLocationContext"
+import customMapStyles from "./customMapStyles"
 
 const LATITUDE_AND_LONGITUDE_DELTA = 0.015
 
 export default function AnimalLocation() {
+  const systemColorScheme = useColorScheme()
+
+  const customMapStyle = customMapStyles[systemColorScheme || "light"]
+
   return (
     <AnimalLocationProvider>
       <SafeAreaViewContent>
         <MapView
           style={mapViewStyles}
+          customMapStyle={customMapStyle}
           region={{
             latitude: 37.78825,
             longitude: -122.4324,
