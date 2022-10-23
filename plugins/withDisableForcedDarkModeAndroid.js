4e7c94f15e85ca369c562fb0f83503c1c1157204
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2022-10-23 14:59:33 -0300

    Ajustes

--- plugins/withDisableForcedDarkModeAndroid.js
+++ plugins/withDisableForcedDarkModeAndroid.js
@@ -13,7 +13,7 @@ function setForceDarkModeToFalse(styles) {
     add: true,
     parent: AndroidConfig.Styles.getAppThemeLightNoActionBarGroup(),
     name: `android:forceDarkAllowed`,
-    value: "false",
+    value: false,
   })
 
   return styles
